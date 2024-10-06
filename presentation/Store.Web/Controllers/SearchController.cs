dfde54ceb9c1a5edbed42f41392cfecd704fe3f7
Sofia <lanovskasofia@gmail.com>
2024-10-06 13:35:40 +0300

    Додати сторінку пошуку

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kyrsova;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+
+        public SearchController(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                return View(new List<Book>());
+            }
+
+            var books = bookRepository.GetAllByTitle(query);
+
+            return View(books);
+        }
+    }
+}
+
